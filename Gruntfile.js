4f19ec4208600e3791c2bf139f1b42ba8742e7b2
Egor Rukhlov <ruhlov.egor@yandex.ru>
2026-01-12 16:57:51 +0500

    feat: boilerplate for grunt

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+const sass = require('sass');
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      build: ['build/']
+    },
+
+    sass: {
+      dev: {
+        options: {
+          implementation: sass,
+          sourceMap: true
+        },
+        files: { 'build/css/style.css': 'assets/scss/main.scss' }
+      },
+      prod: {
+        options: {
+          implementation: sass,
+          sourceMap: false
+        },
+        files: { 'build/css/style.css': 'assets/scss/main.scss' }
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 11'],
+        map: true
+      },
+      dist: {
+        src: 'build/css/style.css',
+        dest: 'build/css/style.css'
+      }
+    },
+
+    cssmin: {
+      prod: {
+        files: { 'build/css/style.css': ['build/css/style.css'] }
+      }
+    },
+
+    concat: {
+      dist: {
+        src: ['assets/js/**/*.js'],
+        dest: 'build/js/scripts.js'
+      }
+    },
+    uglify: {
+      prod: {
+        src: 'build/js/scripts.js',
+        dest: 'build/js/scripts.js'
+      }
+    },
+
+    htmlmin: {
+      prod: {
+        options: { removeComments: true, collapseWhitespace: true },
+        files: { 'build/index.html': 'index.html' }
+      },
+      dev: {
+        files: { 'build/index.html': 'index.html' }
+      }
+    },
+
+    copy: {
+      fonts: {
+        expand: true,
+        cwd: 'assets/fonts/',
+        src: '**',
+        dest: 'build/assets/fonts/'
+      },
+      images: {
+        expand: true,
+        cwd: 'assets/img/',
+        src: '**',
+        dest: 'build/assets/img/'
+      }
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'build/css/*.css',
+            'build/js/*.js',
+            'build/index.html',
+            'build/assets/img/**/*'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: './build'
+        }
+      }
+    },
+
+    watch: {
+      html: { files: ['index.html'], tasks: ['htmlmin:dev'] },
+      css: { files: ['assets/scss/**/*.scss'], tasks: ['sass:dev', 'autoprefixer'] },
+      js: { files: ['assets/js/**/*.js'], tasks: ['concat'] },
+      img: { files: ['assets/img/**/*'], tasks: ['copy:images'] }
+    }
+  });
+
+  grunt.registerTask('dev', [
+    'clean', 'htmlmin:dev', 'sass:dev', 'autoprefixer', 'concat', 'copy:fonts', 'copy:images', 'browserSync', 'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean', 'htmlmin:dev', 'sass:dev', 'autoprefixer', 'concat', 'copy:fonts', 'copy:images'
+  ]);
+
+  grunt.registerTask('prod', [
+    'clean', 'htmlmin:prod', 'sass:prod', 'autoprefixer', 'cssmin', 'concat', 'uglify', 'copy:fonts', 'copy:images'
+  ]);
+
+  grunt.registerTask('default', ['dev']);
+};
